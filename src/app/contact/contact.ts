21823c4623993d94e7097b7ff2170de4b0bba7fa
swertheim1 <115668509+swertheim1@users.noreply.github.com>
2023-11-05 12:15:31 -0600

    updated contact page

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardTitle, IonCardContent, IonCardHeader, IonList, IonCardSubtitle, IonInput, IonItem, IonTextarea, IonButton} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { IonRippleEffect } from '@ionic/angular';
+// import { IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-contact',
   templateUrl: 'contact.html',
   styleUrls: ['contact.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonCard, IonCardTitle, IonCardContent, IonCardHeader, IonCardSubtitle,
+  IonList, IonInput, IonItem, IonTextarea, IonButton ],
 })
 export class Contact {
   constructor() {}
