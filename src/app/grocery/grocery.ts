0d06b6a0be3033729c831e79423088385e079153
swertheim1 <115668509+swertheim1@users.noreply.github.com>
2023-11-05 11:21:25 -0600

    Added Grocery list items to grocery pg

--- src/app/grocery/grocery.ts
+++ src/app/grocery/grocery.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-import { IonAvatar, IonItem, IonLabel, IonList, IonReorderGroup } from '@ionic/angular/standalone';
+import { IonAvatar, IonItem, IonLabel, IonList, IonReorderGroup, IonItemSliding, IonItemOption, IonItemOptions, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-grocery',
   templateUrl: 'grocery.html',
   styleUrls: ['grocery.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonList, IonLabel, IonItem, IonReorderGroup, IonAvatar]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonList, IonLabel, IonItem, IonReorderGroup, IonAvatar, IonItemSliding, IonItemOption, IonItemOptions, IonGrid, IonRow, IonCol]
   
 })
 export class Grocery {
